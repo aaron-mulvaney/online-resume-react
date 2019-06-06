e2b5f53c78a332f7a3c240eb1acf0197d213caba
mulvaney08 <aaronmulvaney@gmail.com>
2019-06-06 14:50:41 +0100

    Spacing change in experience

---
+++ online-resume-react-master/src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div id="contact_container">
+        <h1>Contact</h1>
+        <hr id="tophr"></hr>
+        <hr></hr>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ online-resume-react-master/src/I&H.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import './I&H.css';
+
+class IH extends Component {
+  render() {
+    return (
+      <div id="ih_container">
+        <h1>Interests & Hobbies</h1>
+        <hr id="tophr"></hr>
+        <hr></hr>
+      </div>
+    );
+  }
+}
+
+export default IH;
