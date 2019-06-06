f57b48495e606b10f142ef16253b9bda78cf385c
mulvaney08 <aaronmulvaney@gmail.com>
2019-06-06 15:17:03 +0100

    testing mobile number

--- src/Profile.js
+++ src/Profile.js
@@ -38,10 +38,10 @@ class Profile extends Component {
             <a href="https://goo.gl/maps/JNsw4QZnLKF58eP58" target="_blank"><i class="fa fa-location-arrow"></i> Dublin, Ireland</a>
             </p>
             <div id="soc-buttons">
-              <a class="fa fa-lin fa-linkedin-square" href="https://www.linkedin.com/in/aaronmulvaney/" />
-              <a class="fa fa-github fa-github" href="https://github.com/mulvaney08" />
-              <a class="fa fa-goo fa-google" href="mailto://aaronmulvaney@gmail.com" />
-              <i class="fa fa-phone fa-phone"><a class="phone_tip" data-toggle="tooltip" title data-original-title="+353858241413" href="tel:+353858241413"/></i>            
+              <a class="fa fa-lin fa-linkedin-square" target="_blank" href="https://www.linkedin.com/in/aaronmulvaney/" />
+              <a class="fa fa-github fa-github" target="_blank" href="https://github.com/mulvaney08" />
+              <a class="fa fa-goo fa-google" target="_blank" href="mailto://aaronmulvaney@gmail.com" />
+              <i class="fa fa-phone fa-phone"><a class="phone_tip" data-toggle="tooltip" title data-original-title="+353-85-824-1413" href="tel:+353-85-824-1413"/></i>            
             </div>
           </div>
         </div>
